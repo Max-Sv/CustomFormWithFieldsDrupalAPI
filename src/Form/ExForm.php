8f3a7f9867877ea0c4ff3031b61530c2f3a808ee
Max <mlalx@tut.by>
2019-02-25 17:39:27 +0300

    second commit

--- src/Form/ExForm.php
+++ src/Form/ExForm.php
@@ -59,7 +59,7 @@ class ExForm extends FormBase {
   $form['submit'] = [
    '#type' => 'submit',
    '#name' => 'submit',
-   '#value' =>'Submit',
+   '#value' =>'Submit this form',
   ];
  
   return $form;
@@ -95,11 +95,12 @@ else {
  public function submitForm(array &$form, FormStateInterface $form_state) {
   
   drupal_set_message('Form submitted!');
+  drupal_set_message($form['email']);
  }
  /**
  * {@inheritdoc}
  */
-public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
+/*public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
   $response = new AjaxResponse();
   if (substr($form_state->getValue('email'), -11) == 'example.com') {
     $response->addCommand(new HtmlCommand('.email-validation-message', 'This provider can lost our mail. Be care!'));
@@ -109,6 +110,6 @@ public function validateEmailAjax(array &$form, FormStateInterface $form_state) 
     $response->addCommand(new HtmlCommand('.email-validation-message', ''));
   }
   return $response;
-}
+}*/
  
 }
